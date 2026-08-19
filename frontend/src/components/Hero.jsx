f83f39db24000c65c7693819f189b5d14b49f417
root <root@agent-env-1f7349c8-d73c-447b-a444-bd9df4dae222>
2026-08-19 07:11:17 +0000

    Move HomeEase section before footer

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -313,7 +313,6 @@ export default function Hero() {
           </button>
         ))}
       </div>
-      <HomeEaseBand />
     </section>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -40,6 +40,7 @@ export default function Home() {
       <Leadership />
       <Customize />
       <Contact />
+      <HomeEaseBand />
     </>
   );
 }
